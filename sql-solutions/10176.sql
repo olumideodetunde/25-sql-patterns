4b62f54c8558b4f48bdbf7e2b6e7b1fdfaa3a5c4
Olumide Odetunde <odetundeolumide95@gmail.com>
2026-01-16 06:52:51 +0000

    10176 solved

--- sql-solutions/10176.sql
+++ sql-solutions/10176.sql
@@ -1,3 +1,4 @@
+--Link: https://platform.stratascratch.com/coding/10176-bikes-last-used?code_type=1
 SELECT
     bike_number,
     MAX(end_time) AS last_used
@@ -5,4 +6,4 @@ FROM
     dc_bikeshare_q1_2012
 GROUP BY
     bike_number
-ORDER BY MAX(end_time)
+ORDER BY MAX(end_time);
