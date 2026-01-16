54960fde086b9332d52d18236c8fc457da6d5511
Olumide Odetunde <odetundeolumide95@gmail.com>
2026-01-16 07:21:31 +0000

    10351 solved

---
+++ sql-solutions/10351.sql
@@ -0,0 +1,19 @@
+--Link: https://platform.stratascratch.com/coding/10351-activity-rank?code_type=1
+
+--Count emails sents for each user
+WITH email_sent_table AS(
+    SELECT
+        from_user as user_id,
+        COUNT(from_user) as total_email
+    FROM
+        google_gmail_emails
+    GROUP BY from_user
+)
+
+-- Rank the table using total_email in descending order and user id to break ties
+SELECT
+    user_id,
+    total_email,
+    DENSE_RANK() OVER (ORDER BY total_email DESC, user_id ASC) as activity_rank
+FROM
+    email_sent_table;
